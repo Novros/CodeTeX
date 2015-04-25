98c159f1b3c1aba130373c57e6e6c238b9cc85f3
Rostislav Novak <rostislav.novak92@gmail.com>
2015-04-26 00:24:31 +0200

    Init maven project.

---
+++ src/main/java/cz/novros/tex/semestral_work/Main.java
@@ -0,0 +1,10 @@
+package cz.novros.tex.semestral_work;
+
+/**
+ * Created by rostislav on 26.4.15.
+ */
+public class Main {
+    public static void main(String [] args) {
+        System.out.println("Hello");
+    }
+}
